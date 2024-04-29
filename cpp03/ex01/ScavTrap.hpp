0dd1d6729c50b5457c7b5d1e3a29570dcff3115b
jbidaux <69929476+Meltallian@users.noreply.github.com>
2024-04-29 18:27:29 +0200

    Update ScavTrap.hpp

--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -1,3 +1,21 @@
 #pragma once
 
 #include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	private:
+
+	bool	_guardMode;
+
+	public:
+
+	ScavTrap(std::string name);
+	~ScavTrap();
+	ScavTrap &operator=(const ScavTrap &other);
+	ScavTrap(const ScavTrap &other);
+
+	void	guardGate();
+	bool	getGuardMode();
+	void	setGuardMode(bool activate);
+};

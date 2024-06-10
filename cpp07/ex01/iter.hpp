029fbc28d2a63b765df9b09cf9b4e8ed72fe0e49
Jeremie Bidaux <jbidaux@c3r2s4.42lausanne.ch>
2024-06-10 18:52:21 +0200

    ex 01 cpp07 done

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -4,57 +4,11 @@
 #include <iostream>
 #include <cmath>
 
-//"For any two types T and U, set value to false."
-template <typename T, typename U>
-struct is_same {
-    static const bool value = false;
-};
-
-//This specialization overrides the primary template for the case 
-//when both type parameters are the same, setting value to true.
-template<typename T>
-struct is_same<T, T> {
-    static const bool value = true;
-};
-//When you instantiate is_same with two types, the compiler 
-//will use the appropriate definition 
-//(primary or specialized) to set the value of value.
-template <typename T, typename U>
-bool are_same_type(const T&, const U&) {
-    return is_same<T, U>::value;
-}
-
-
-template <typename T, typename U>
-void	swap(T &a, U &b)
+template <typename T, typename U, typename F>
+void	iter(T *array, U len, F f)
 {
-	if (are_same_type(a,b))
+	for (U i = 0; i < len; i++)
 	{
-		T tmp;
-		tmp = a;
-		a = b;
-		b = tmp;
+		f(array[i]);
 	}
-	else
-		std::cout << "Both value types aren't the same" << std::endl;
-}
-
-template <typename T>
-T	min(T &a, T &b)
-{
-
-	if (a < b)
-		return a;
-	else
-		return b;
-}
-
-template <typename T>
-T	max(T &a, T &b)
-{
-	if (a > b)
-		return a;
-	else
-		return b;	
-
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,7 +1,20 @@
 #include "iter.hpp"
 
+// Example function to be used with iter
+void printElement(int elem)
+{
+    std::cout << elem << " ";
+}
+
 int main()
 {
-	
-	return 0;
+    int arr[] = {1, 2, 3, 4, 5};
+    std::size_t len = sizeof(arr) / sizeof(arr[0]);
+
+    // Using iter with a function pointer
+    iter(arr, len, printElement);
+    std::cout << std::endl;
+
+
+    return 0;
 }

793389304f1654f1dae42fe0258cacb9a8181c7b
Jeremie Bidaux <jeremie.bidaux@gmail.com>
2024-06-19 13:05:02 +0200

    ex00 cpp09 done

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,6 +1,106 @@
 #include "BitcoinExchange.hpp"
 
+bool	is_line_valid(std::string line)
+{
+	if (line == "date | value")
+		return false;
+	if (line.empty())
+	{
+		std::cout << "Error: line is empty" << std::endl;
+		return false;		
+	}
+	if (!line.find('|'))
+	{
+		std::cout << "Error: bad input " << line << std::endl;
+		return false;		
+	}
+	return true;
+}
 
+bool	is_value_valid(double value)
+{
+	if (value > 1000)
+	{
+		std::cout << "Error: too large a number : " << value << std::endl;
+		return false;		
+	}
+	if (value < 0)
+	{
+		std::cout << "Error: not a positive number : " << value << std::endl;
+		return false;		
+	}
+	return true;
+}
+
+bool	is_date_valid(std::string date)
+{
+	if (date < "2009-01-02")
+	{
+		std::cout << "Error: bad input => " << date << std::endl;
+		return false;
+	}
+	if (date.empty())
+	{
+		std::cout << "Error: no date." << std::endl;
+		return false;
+	}
+	if (!date.find('-'))
+	{
+		std::cout << "Error: bad input => " << date << std::endl;
+		return false;		
+	}
+	std::istringstream iss(date);
+	double	year;
+	double	month;
+	double	day;
+    char delimiter;
+
+    iss >> year;
+    iss >> delimiter;  // Read and discard the hyphen
+    iss >> month;
+    iss >> delimiter; // Read and discard the hyphen
+    iss >> day;
+
+	if (month < 1 || month > 12)
+	{
+		std::cout << "Error: bad month input => " << date << std::endl;
+		return false;			
+	}
+	if (day < 1 || month > 31)
+	{
+		std::cout << "Error: bad day input => " << date << std::endl;
+		return false;			
+	}
+	return true;
+}
+
+void	exchange(BitcoinExchange &coin, std::fstream &infile)
+{
+	std::string	line;
+	while (std::getline(infile, line))
+	{
+		if (!is_line_valid(line))
+			continue;
+		if (!is_date_valid(line))
+			continue;
+		std::istringstream iss(line);
+		std::string date;
+		double value;
+		std::getline(iss, date, '|');
+		iss >> value;
+		if (!is_value_valid(value))
+			continue;
+   		if (iss.fail())
+		{
+       		std::cout << "Error: bad value syntax." << std::endl;
+			continue;
+		}
+		BitcoinExchange::iterator it = coin.lower_bound(date);
+		it--;
+		double result = it->second * value;
+		std::cout << date << " => " << value << " = " << result << std::endl;
+	}
+}
 
 int main(int ac, char **av)
 {
@@ -16,7 +116,7 @@ int main(int ac, char **av)
 		if (!data)
 			std::cerr << "Error: 'data.csv' corrupted." << std::endl;
 		
-		BitcoinExchange	*Coin = new BitcoinExchange;
+		BitcoinExchange	Coin;
 		std::string line;
 		while (std::getline(data, line)) 
 		{
@@ -40,11 +140,11 @@ int main(int ac, char **av)
 
 			// Insert into the multimap
 			if (!(date.size() < 9)) //to remove first line
-				Coin->insert(std::make_pair(date, value));
+				Coin.insert(std::make_pair(date, value));
     	}
-		for (BitcoinExchange::iterator it = Coin->begin(); it != Coin->end(); ++it) {
-        std::cout << "Date: " << it->first << ", Value: " << it->second << std::endl;
-    	}
+		// for (BitcoinExchange::iterator it = Coin.begin(); it != Coin.end(); ++it) 
+       	// 	std::cout << "Date: " << it->first << ", Value: " << it->second << std::endl;
+		exchange(Coin, infile);
 	}
 	else
 	{
